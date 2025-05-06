902923c47be67b80c83b014e6ac2ea4ee15946c8
Mati Joseph <crucialniccur@gmail.com>
2025-05-06 20:19:50 +0300

    define the happy_new_year function

--- lib/looping.py
+++ lib/looping.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+        print('Happy New Year!')
+
 
 def square_integers(int_list):
     # code goes here!
     pass
+
 
 def fizzbuzz():
     # code goes here!
